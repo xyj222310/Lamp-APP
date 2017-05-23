1f77349a88a60566eea751407f42bc531a576c12
DESKTOP-I5JE1LC\XieYingjie <xyj222310@163.com>
2017-05-23 17:42:35 +0800

    add:二维码扫描功能

--- Lamp/app/src/main/java/com/yjtse/lamp/MainActivity.java
+++ Lamp/app/src/main/java/com/yjtse/lamp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.yjtse.lamp;
 
+import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.client.android.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent openCameraIntent = new Intent(this, CaptureActivity.class);
+        startActivityForResult(openCameraIntent, 0);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 0) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            Log.d("TAG",scanResult);
+        }
+    }
+
 }
